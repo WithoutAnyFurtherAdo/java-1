9b871c54434d25eac9f95eb0956a5673b990628a
Bilal Ado <1bilalado@gmail.com>
2026-09-10 23:54:36 +0100

    done.

---
+++ ChapterOneQuestionThree.java
@@ -0,0 +1,8 @@
+public class ChapterOneQuestionThree {
+  public static void main(String args[]) {
+    System.out.println("   J    A    v     v    A    ");
+    System.out.println("   J   A A    v   v    A A   ");
+    System.out.println("J  J  AAAAA    v v    AAAAA  ");
+    System.out.println(" JJ  A     A    v    A     A ");
+  }
+}
