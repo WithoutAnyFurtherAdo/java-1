58a238dec28b857bc142c1b6a2d8c31016909a7e
Bilal Ado <1bilalado@gmail.com>
2026-09-17 23:13:38 +0100

    Correction.

--- ChapterOneQuestionTen.java
+++ ChapterOneQuestionTen.java
@@ -1,5 +1,6 @@
+// i forgot i had to divide the mile value by time in hours
 public class ChapterOneQuestionTen {
   public static void main(String args[]) {
-   System.out.println(14 / 1.6);
+   System.out.println( (14 / 1.6) / ( ( (45 * 60) + 30 ) / (60.0 * 60.0) ) );
   }
 }
