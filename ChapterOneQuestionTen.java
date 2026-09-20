47b71d4bd8350fce446887e04e9d63a585cf0365
Bilal Ado <1bilalado@gmail.com>
2026-09-20 12:27:08 +0100

    correction.

--- ChapterOneQuestionTen.java
+++ ChapterOneQuestionTen.java
@@ -1,4 +1,4 @@
-// i forgot i had to divide the mile value by time in hours
+// i forgot i had to divide the mile value by time in hours and also the decimal truncation feature
 public class ChapterOneQuestionTen {
   public static void main(String args[]) {
    System.out.println( (14 / 1.6) / ( ( (45 * 60) + 30 ) / (60.0 * 60.0) ) );
