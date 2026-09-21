4673062b47f778c03dfd25a67552a56d99323b09
Bilal Ado <1bilalado@gmail.com>
2026-09-21 23:22:19 +0100

    done.

---
+++ ChapterOneQuestionTwelve.java
@@ -0,0 +1,6 @@
+// still forgetting integer division
+public class ChapterOneQuestionTwelve {
+  public static void main(String args[]) {
+    System.out.println( (24 * 1.6) / ( ((60 * 60) + (40 * 60) + (35)) / (60.0 * 60)) );
+  }
+}
